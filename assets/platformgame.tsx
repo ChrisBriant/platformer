d5a670818089c32d86aeeea2921bd503ab046bc2
Chris Briant <cbriant@btinternet.com>
2020-03-09 06:29:35 +0000

    First commit

---
+++ assets/platformgame.tsx
@@ -0,0 +1,27 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.3" name="tilemap" tilewidth="30" tileheight="30" tilecount="12" columns="4">
+ <image source="tilemap.png" width="120" height="90"/>
+ <terraintypes>
+  <terrain name="New Terrain" tile="1"/>
+ </terraintypes>
+ <tile id="1">
+  <objectgroup draworder="index" id="2">
+   <object id="1" name="wood" x="0" y="0" width="30" height="30"/>
+  </objectgroup>
+ </tile>
+ <tile id="4">
+  <objectgroup draworder="index" id="2">
+   <object id="1" name="ground" x="0" y="0" width="30" height="30"/>
+  </objectgroup>
+ </tile>
+ <tile id="5">
+  <objectgroup draworder="index" id="2">
+   <object id="1" name="ground" x="0" y="0" width="30" height="30"/>
+  </objectgroup>
+ </tile>
+ <tile id="6">
+  <objectgroup draworder="index" id="2">
+   <object id="1" name="ground" x="0" y="0" width="30" height="30"/>
+  </objectgroup>
+ </tile>
+</tileset>
